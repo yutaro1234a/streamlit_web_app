eaa01dc00c7bc8e2a11b743d30512500f31ae0b8
a.yutaro <y9t7r.a.05iiw@gmail.com>
2023-06-12 17:37:42 +0900

    修正

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pandas as pd
 import copy
 
 st.title('ランニングスコア')
-st.header('得点・サポート登録画面')
-st.subheader('得点・サポート登録画面')
-st.caption('得点・サポートなどを行った選手を登録して下さい。')
+st.header('得点・アシスト集計画面')
+st.caption('得点・アシストなどを行った選手を登録して下さい。')
+
+markdown = """
+\n
+"""
 
 @st.cache_resource
 def cache_lst():
@@ -14,55 +17,82 @@ def cache_lst():
 lst = cache_lst()
 
 with st.form (key = 'input_form'):
- team = st.radio('TEAM', ('Red', 'Blue'), horizontal=True)
  classType= st.radio('CLASS', ('初級', '中級'), horizontal=True)
- bibsType = st.radio('ビブスType', ('無地', '無地以外'), horizontal=True)
- uniformNumber = st.selectbox('背番号', ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10'))
- option = st.selectbox('種類', ('ツーポイント', 'スリーポイント', 'フリースロー', 'アシスト', 'ブロック', 'リバウンド', 'スティール', 'ファール'))
- markdown = """ 
- """
  st.write(markdown)
- col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+ team = st.radio('TEAM', ('Red', 'Blue'), horizontal=True)
+ st.write(markdown)
+ bibsType = st.radio('ビブスType', ('ドバスOriginal', 'SPALDING', '無地'), horizontal=True)
+ st.write(markdown)
+ uniformNumber = st.selectbox('背番号', ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '21', '31', '32', '35'))
+ st.write(markdown)
+ option = st.selectbox('得点・アシスト', ('ツーポイント', 'スリーポイント', 'フリースロー', 'アシスト', 'ブロック', 'リバウンド', 'スティール', 'ファール'))
+ st.write(markdown)
+ 
+ col1, col2, col3 = st.columns([2, 1, 1])
  with col1:
   submit_btn = st.form_submit_button('登録')
  with col2:
-  submit_btn2 = st.form_submit_button('一覧表示')
+  listAll_btn = st.form_submit_button('一覧表示')
  with col3:
-  submit_btn3 = st.form_submit_button('最終行を削除')
- with col4:
-  clear_btn = st.form_submit_button('ALLクリア')
+  DeleteLastRow_btn = st.form_submit_button('最終行を削除')
  
- if submit_btn2: 
-     df = pd.DataFrame(lst)
-     df.columns = ['TEAM', 'CLASS', 'ビブスType', '背番号', '種類']
-     st.dataframe(df)
-    #  st.write(lst)
+ if submit_btn:
+     try:
+      lst.append([classType, team, bibsType, uniformNumber, option])
+      df = pd.DataFrame(lst)
+      df.columns = ['CLASS', 'TEAM', 'ビブスType', '背番号', '得点・アシスト']
+      st.dataframe(df)
+      #  st.write(lst)
+     except:
+      # エラーメッセージ
+      st.error('データ登録時にエラーが発生しました。')
+      
+ if listAll_btn:
+     try:
+      df = pd.DataFrame(lst)
+      df.columns = ['CLASS', 'TEAM', 'ビブスType', '背番号', '得点・アシスト']
+      st.dataframe(df)
+      #  st.write(lst)
+     except:
+      # エラーメッセージ
+      st.warning('表示するデータがありません。データを登録して下さい')
 
- if submit_btn: 
-     lst.append([team, classType, bibsType, uniformNumber, option])
-     df = pd.DataFrame(lst)
-     df.columns = ['TEAM', 'CLASS', 'ビブスType', '背番号', '種類']
-     st.dataframe(df)
-    #  st.write(lst)
+ if DeleteLastRow_btn: 
+     try:
+      lst.pop(-1)
+      df = pd.DataFrame(lst)
+      df.columns = ['CLASS', 'TEAM', 'ビブスType', '背番号', '得点・アシスト']
+      st.dataframe(df)
+     #  st.write(lst)
+     except:
+     # エラーメッセージ
+      st.warning('表示するデータがありません。データを登録して下さい')
 
- if submit_btn3: 
-     lst.pop(-1)
-     df = pd.DataFrame(lst)
-     df.columns = ['TEAM', 'CLASS', 'ビブスType', '背番号', '種類']
-     st.dataframe(df)
-    #  st.write(lst)
+check = st.checkbox('最終行以外を削除する場合はチェックする')
+if check: 
+ delete_num = st.text_input('削除する行番号を入力して削除ボタンを押下')
+ col1, col2 = st.columns([4, 1])
+ with col1:
+  delete_btn = st.button('削除')
+ with col2:
+  clear_btn = st.button('すべてクリア')
 
- if clear_btn: 
-     st.cache_resource.clear()
-
-check = st.checkbox('最終行以外を削除する場合はチェックをつける')
-if check:
- delete_num = st.text_input('削除する行番号を入力')
- delete_btn = st.button('削除')
- 
  if delete_btn:
-     lst.pop(int(delete_num))
+     try:
+      lst.pop(int(delete_num))
+     except:
+      st.error('データ削除時にエラーが発生しました。数値で入力するが存在する行番号を設定して下さい。')
 
+ if clear_btn:
+     try:
+      st.cache_resource.clear()
+     except:
+      st.error('データ削除時にエラーが発生しました。')
+ 
+ st.write(markdown)
+ st.write(markdown)
+ 
+st.write(markdown)
 submit_btn4 = st.button('集計')
 if submit_btn4:
  lst2 = []
@@ -75,7 +105,7 @@ if submit_btn4:
  lstF = []
  strlist = []
  sumlist = []
- 
+
  strlist = copy.deepcopy(lst)
  
  for item in strlist:
@@ -103,79 +133,82 @@ if submit_btn4:
      elif item[4] == 'ファール':
          item[4] = 1
          lstF.append(item)
-          
- df = pd.DataFrame(lst2)
- df = pd.DataFrame(lst2, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計得点'])
- markdown = """
- ツーポイントの結果はこちら↓
- """
- st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+         
+ teamPointList = []
+ tmpPointList = []
+ tmpPointList += lst1 + lst2 + lst3
+ 
+ for item in tmpPointList:
+     teamPointList.append([item[1], item[4]])
 
- df = pd.DataFrame(lst3)
- df = pd.DataFrame(lst3, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計得点'])
  markdown = """
- スリーポイントの結果はこちら↓
+ チームごとの得点の集計結果はこちら ↓
  """
+ df = pd.DataFrame(teamPointList, columns=['TEAM', '合計得点'])
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
-
- df = pd.DataFrame(lst1)
- df = pd.DataFrame(lst1, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計得点'])
- markdown = """
- フリースローの結果はこちら↓
- """
- st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
-
+ st.write(df.groupby(['TEAM']).sum())
 
  sumlist += lst1 + lst2 + lst3
- df = pd.DataFrame(sumlist)
- df = pd.DataFrame(sumlist, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計得点'])
+ df = pd.DataFrame(sumlist, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計得点'])
  markdown = """
- すべての結果はこちら↓
+ すべての得点の集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
-
-
- df = pd.DataFrame(lstA)
- df = pd.DataFrame(lstA, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計回数'])
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
+          
+ df = pd.DataFrame(lst2, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計得点'])
  markdown = """
- アシストの結果はこちら↓
+ ツーポイントのみの集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
 
- df = pd.DataFrame(lstB)
- df = pd.DataFrame(lstB, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計回数'])
+ df = pd.DataFrame(lst3, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計得点'])
  markdown = """
- ブロックの結果はこちら↓
+ スリーポイントのみの集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
 
- df = pd.DataFrame(lstR)
- df = pd.DataFrame(lstR, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計回数'])
+ df = pd.DataFrame(lst1, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計得点'])
  markdown = """
- リバウンドの結果はこちら↓
+ フリースローのみの集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
 
- df = pd.DataFrame(lstS)
- df = pd.DataFrame(lstS, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計回数'])
+ df = pd.DataFrame(lstA, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計回数'])
  markdown = """
- スティールの結果はこちら↓
+ アシストの集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
 
- df = pd.DataFrame(lstF)
- df = pd.DataFrame(lstF, columns=['TEAM', 'CLASS', 'ビブスType', '背番号', '合計回数'])
+ df = pd.DataFrame(lstB, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計回数'])
  markdown = """
- ファールの結果はこちら↓
+ ブロックの集計結果はこちら ↓
  """
  st.write(markdown)
- st.write(df.groupby(['TEAM', 'CLASS', 'ビブスType', '背番号']).sum())
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
+
+ df = pd.DataFrame(lstR, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計回数'])
+ markdown = """
+ リバウンドの集計結果はこちら ↓
+ """
+ st.write(markdown)
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
+
+ df = pd.DataFrame(lstS, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計回数'])
+ markdown = """
+ スティールの集計結果はこちら ↓
+ """
+ st.write(markdown)
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
+
+ df = pd.DataFrame(lstF, columns=['CLASS', 'TEAM', 'ビブスType', '背番号', '合計回数'])
+ markdown = """
+ファールの集計結果はこちら ↓
+ """
+ st.write(markdown)
+ st.write(df.groupby(['CLASS', 'TEAM', 'ビブスType', '背番号']).sum())
 
